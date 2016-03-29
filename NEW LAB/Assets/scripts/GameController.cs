8b023918ebacaf9f8ac168fbf7dffc847003e4c4
worawich.wera <worawich.wera@bumail.net>
2016-03-29 15:17:02 +0700

    Spawning waves

--- NEW LAB/Assets/scripts/GameController.cs
+++ NEW LAB/Assets/scripts/GameController.cs
@@ -3,19 +3,33 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 	public GameObject hazard;
-
-	// Use this for initialization
+	public int hazardCount;
+	public float spawnWaite;
+	public float waveWaite;
+	public float startWaite;
+	public Vector3 spawnValues;	// Use this for initialization
 	void Start () {
-		SpawnWaves();
+		StartCoroutine(SpawnWaves());
 
 	}
 
-	void SpawnWaves(){
-		Vector3 spawnPosition = new Vector3 (0.0f, 0.0f, 5f);
-		Quaternion spawnRotation = Quaternion.identity;
+	IEnumerator SpawnWaves()
+	{
+		yield return new WaitForSeconds(startWaite);
 
+
+		while(true)
+		{
+
+		for(int i =0;i<hazardCount;i++)
+		{
+			Vector3 spawnPosition = new Vector3 (Random.Range(-spawnValues.x,11),spawnValues.y, spawnValues.z);
+		Quaternion spawnRotation = Quaternion.identity;
 		Instantiate (hazard, spawnPosition, spawnRotation);
 
-
+			yield return new WaitForSeconds(spawnWaite);
 	}
+			yield return new WaitForSeconds(waveWaite);
+}
+}
 }
