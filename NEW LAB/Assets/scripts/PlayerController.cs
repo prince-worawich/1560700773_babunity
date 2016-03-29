4945fcc6c3e3f2c66bae0e969e5dc6b6fa81abc1
worawich.wera <worawich.wera@bumail.net>
2016-03-29 22:33:42 +0700

    Audio

--- NEW LAB/Assets/scripts/PlayerController.cs
+++ NEW LAB/Assets/scripts/PlayerController.cs
@@ -56,6 +56,7 @@ public class PlayerController : MonoBehaviour {
 			nextFire=Time.time+fireRate;
 			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
 		
+			GetComponent<AudioSource> ().Play ();
 		}
 	}
 
