397d1974b2d6f233909ffc120178bd61771bd893
worawich.wera <worawich.wera@bumail.net>
2016-03-29 14:53:42 +0700

    Game Controller

---
+++ NEW LAB/Assets/scripts/GameController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	public GameObject hazard;
+
+	// Use this for initialization
+	void Start () {
+		SpawnWaves();
+
+	}
+
+	void SpawnWaves(){
+		Vector3 spawnPosition = new Vector3 (0.0f, 0.0f, 5f);
+		Quaternion spawnRotation = Quaternion.identity;
+
+		Instantiate (hazard, spawnPosition, spawnRotation);
+
+
+	}
+}
