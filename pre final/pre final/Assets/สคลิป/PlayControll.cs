fa0e9261b13cf1d01e202f788c4f79c686263bca
worawich.wera <worawich.wera@bumail.net>
2016-04-26 23:59:19 +0700

    edit2

---
+++ pre final/pre final/Assets/สคลิป/PlayControll.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayControll : MonoBehaviour {
+	private Rigidbody rb;
+	public float speed;
+
+
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal,0.0f,moveVertical);
+		rb.AddForce (movement*speed);
+	
+	}
+
+	void OnTriggerEnter(Collider ghost)
+	{
+		if(ghost.gameObject.CompareTag("item"))
+		{
+			ghost.gameObject.SetActive (false);
+		}
+		}
+}
