abb11a4acd66c3571760862b32b7f8281cc2347e
WORAWICH WERAPATSAKULCHAI <worawich.wera@bulive.net>
2016-04-27 12:53:42 +0700

    final

---
+++ final/New Unity Project/Assets/movement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+	float speedX =0;
+	float speedY = -1;
+	float speedZ =0;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Translate(new Vector3(speedX,speedY,speedZ)*Time.deltaTime);
+	
+	}
+}
---
+++ pre final/pre final/Assets/Gamemain.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor.UI;
+
+public class Gamemain : MonoBehaviour {
+	public GameObject PlayerDeadPrefab;
+	public Rigidbody rb;
+	public GameObject Button1;
+	public GameObject Button2;
+
+	private int count1;
+	private int count2;
+	public Text countText;
+	public Text countLife;
+
+	// Use this for initialization
+	void Start () {
+		Button1.SetActive (false);
+		rb = GetComponent<Rigidbody> ();
+		count1 = 1;
+		count2 = 0;
+		countText.text = "Score: " + count2.ToString ();
+		countLife.text = "Life: " + count1.ToString ();
+
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+
+	void OnCollisionEnter(Collision collision){
+		if (collision.gameObject.tag=="enemy"){
+			Instantiate(PlayerDeadPrefab,this.gameObject.transform.position,Quaternion.identity);
+			Destroy(this.gameObject);
+			count1=count-1;
+			countLife.text="Life:"+count.ToString();
+			Button1.SetActive(true);
+			Button2.SetActive(true);
+
+}
+		else if (collision.gameObject.tag ==("point")){
+			count2= count2 + 100;
+			countText.text="Score : " +count2.ToString();
+		}
+}
