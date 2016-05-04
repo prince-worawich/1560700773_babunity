7b3c22ef487bda09c2ac4af0a5921f6c75338c6e
worawich <prince-worawich@hotmail.com>
2016-05-04 17:22:05 +0700

    final 1

--- final/New Unity Project/Assets/movement.cs
+++ final/New Unity Project/Assets/movement.cs
@@ -1,19 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class movement : MonoBehaviour {
+public class movement : MonoBehaviour 
+{
+	public int maxSpeed;
 
-	float speedX =0;
-	float speedY = -1;
-	float speedZ =0;
-	void Start () {
-	
+	private Vector3 startPosition;
+
+	// Use this for initialization
+	void Start () 
+	{
+		maxSpeed = 3;
+
+		startPosition = transform.position;
 	}
-	
-	// Update is called once per frame
-	void Update () {
 
-		transform.Translate(new Vector3(speedX,speedY,speedZ)*Time.deltaTime);
-	
+	// Update is called once per frame
+	void Update ()
+	{
+		MoveVertical ();
+	}
+
+	void MoveVertical()
+	{
+		transform.position = new Vector3(transform.position.x, startPosition.y + Mathf.Sin(Time.time * maxSpeed), transform.position.z);
+
+		if(transform.position.y > 1.0f)
+		{
+			transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+		}
+		else if(transform.position.y < -1.0f)
+		{
+			transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+		}
 	}
 }
---
+++ final/New Unity Project/Assets/ying.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class ying : MonoBehaviour {
+	public Rigidbody projectile;
+	public Transform shots;
+	public int shotw;
+	public int speed;
+	public Button Button;
+	public Button Buttons;
+	public Text Game;
+	public int ball;
+	public Text bull;
+	bool canjump;
+	int jumpcount=0;
+	void Start()
+	{
+		ball = 3;
+		bullet ();
+		Button.gameObject.SetActive (false);
+		Buttons.gameObject.SetActive (false);
+		Game.gameObject.SetActive (false);
+	}
+	void Update()
+	{
+	}
+	public void Shooting()
+	{
+		if(ball==0){
+			Button.gameObject.SetActive (true);
+			Buttons.gameObject.SetActive (true);
+			Game.gameObject.SetActive (true);
+		}
+		if (canjump || jumpcount <1) {
+			Rigidbody shot = Instantiate (projectile, shots.position, shots.rotation) as Rigidbody;
+			shot.velocity = new Vector3 (1, 1, 0) * shotw;
+			jumpcount++;
+			ball--;
+			bullet ();
+		}
+	}
+	void bullet(){
+		bull.text = "Count:" + ball.ToString ();
+
+	}
+	void OnCollisionEnter(Collision other) {
+		if (other.gameObject.tag== "enemy")
+		{
+			canjump = true;
+			jumpcount =0;
+		}
+	}
+	void OnCollisionExit(Collision other) {
+		if (other.gameObject.tag== "enemy")
+		{
+			canjump = false;
+		}
+	}
+}
