01c08eecbebeb7449bc9b0b1dad31b1b71bf37cb
worawich <prince-worawich@hotmail.com>
2016-05-04 18:54:30 +0700

    final3

---
+++ final/New Unity Project/Assets/scitp/boom.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class boom : MonoBehaviour {
+	bool canjump;
+	int jumpcount=0;
+	void Start ()
+	{
+		Rigidbody rb = GetComponent<Rigidbody> ();
+	}
+
+	void OnCollisionEnter(Collision other) 
+	{
+		if (other.gameObject.tag== "enemy")
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ final/New Unity Project/Assets/scitp/yingg.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class yingg : MonoBehaviour {
+	public float speed;
+	public float tilt;
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	private float nextFire;
+	private int count;
+	private int count1;
+	public Text countBullet;
+	public Text countScore;
+	// Use this for initialization
+	void Start () {
+		count = 1;
+		count1 = 10;
+		countBullet.text = "Bullet : " +count1.ToString ();
+		countScore.text = "Score : " +count.ToString ();
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetButton("Fire1") && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+			count1 = count1 - 1;
+			countBullet.text = "Bullet : " + count1.ToString ();
+		}
+	}
+}
